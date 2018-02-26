cff91451cb488a2d2e07935ec883810540842e94
pedroarias1015 <pedroarias1015@gmail.com>
2018-02-26 19:42:08 +0100

    Update main.c

--- 7/src/main.c
+++ 7/src/main.c
@@ -20,10 +20,6 @@ volatile int tiempo_sin_pulsar=0;
 volatile int tiempo_reset=0;
 int t_antirrebote=0;
 
-
-
-
-
 /*
 Enumeración de los estados de la máquina de Mealy
 */
@@ -34,12 +30,6 @@ ARMADO,
 NO_ARMADO,
 };
 
-void imprime(fsm_t* this){
-  printf("%s %d\n",'valor de c:',c);
-  printf("%s  %d \n",'valor de indice:', indice);
-
-
-}
 
   /*
 Definimos la matriz de transicion que tiene como formato
@@ -47,65 +37,48 @@ Definimos la matriz de transicion que tiene como formato
   */
 
 
-int pulsacion(fsm_t* this){
-  //printf("%s  \n","entró");
-
-  if(!GPIO_INPUT_GET(0)){
-
-
-
-    if(xTaskGetTickCount() > t_antirrebote){
-
-
-      t_antirrebote=xTaskGetTickCount() + ANTIRREBOTE;
-      tiempo_sin_pulsar=xTaskGetTickCount() + SEGUNDO;
-      tiempo_reset=xTaskGetTickCount() + 3*SEGUNDO;
-      c++;
-      printf("%s  %d\n","Valor de c:" , c);
-      printf("%s  %d\n","Valor de indice:" , indice);
-      return 1;
-
-    }
-  }return 0;
-}
-
-int codigo_c(fsm_t* this){
-  int val=0;
-  int i;
-
-for(i=0;i<3;i++){
-  if(codigo_introducido[i] != codigo_correcto[i]){
-    val=0;
-    break;
-  }val=1;
-}
-return val;
-}
 
 
 
 
 
-  /*Método que mide si ha pasado más de un segundo entre pulsación
-  */
+
+  int pulsacion(fsm_t* this){
+
+
+    if(!GPIO_INPUT_GET(0)){
+
+
+
+      if(xTaskGetTickCount() > t_antirrebote){
+
+
+        t_antirrebote=xTaskGetTickCount() + ANTIRREBOTE;
+        tiempo_sin_pulsar=xTaskGetTickCount() + SEGUNDO;
+        tiempo_reset=xTaskGetTickCount() + 3*SEGUNDO;
+        c++;
+        printf("%s  %d\n","Valor de c:" , c);
+        printf("%s  %d\n","Valor de indice:" , indice);
+        return 1;
+
+      }
+    }return 0;
+  }
 
 int tiempo_maximo(fsm_t* this){
 
 
-if(xTaskGetTickCount() > tiempo_sin_pulsar){
+  if(xTaskGetTickCount()>tiempo_sin_pulsar){
 
-    if(xTaskGetTickCount() > tiempo_reset){
-      indice=0;
-      printf("%s  %d \n",'valor de indice:', indice);
-
-    }
-
-return 1;
-
+      if(xTaskGetTickCount() > tiempo_reset){
+        indice=0;
+        
+      }
+        return 1;
+  }
+  return 0;
 }
-return 0;
 
-}
 
 
 
@@ -137,17 +110,22 @@ else{
 printf("%s  %d\n","Valor de indice:" , indice);
 
 }
-
-
-
 }
 
 
+int codigo_c(fsm_t* this){
 
+  int val=0;
+  int i;
 
-
-
-
+for(i=0;i<3;i++){
+  if(codigo_introducido[i] != codigo_correcto[i]){
+    val=0;
+    break;
+  }val=1;
+}
+return val;
+}
 
 
 void armar(fsm_t* this){
@@ -166,6 +144,9 @@ codigo_introducido[0]=0; codigo_introducido[1]=0;codigo_introducido[2]=0;
 
 
 }
+
+
+
 
 int comprobar_presencia_1(fsm_t* this){
 
@@ -202,9 +183,6 @@ return 0;
 
 
 
-
-
-
 static fsm_trans_t matriz_transicion1[] = {
 {NO_ARMADO, pulsacion, NO_ARMADO,NULL},
 {NO_ARMADO,tiempo_maximo,NO_ARMADO,clean_codigo},
@@ -215,19 +193,11 @@ static fsm_trans_t matriz_transicion1[] = {
 {-1,NULL,-1,NULL},
 };
 
-
 static fsm_trans_t matriz_transicion2[] = {
 {APAGADO,comprobar_presencia_1,ENCENDIDO,encender},
 {ENCENDIDO,comprobar_presencia_2,APAGADO,apagar},
 {-1,NULL,-1,NULL},
 };
-
-
-
-
-
-
-
 
 
 uint32 user_rf_cal_sector_set(void)
@@ -269,29 +239,26 @@ uint32 user_rf_cal_sector_set(void)
 void inter(void* ignore)
 {
     fsm_t* fsm1 = fsm_new(matriz_transicion1);
-
     fsm_t* fsm2 = fsm_new(matriz_transicion2);
-    desarmar(fsm1);
+    GPIO_OUTPUT_SET(2,1); //Inicialmente arranca el led apagado
     portTickType xLastWakeTime;
 
     while(true) {
+
       xLastWakeTime = xTaskGetTickCount (); //Almacenamos el tiempo actual
-
       fsm_fire(fsm1);
+      if(flag_armar==1){
+      fsm_fire(fsm2);}
 
-if(flag_armar==1){
-        fsm_fire(fsm2);
-      }
       vTaskDelayUntil(&xLastWakeTime, PERIOD_TICK);
     }
 }
 
 void user_init(void)
 {
-      PIN_FUNC_SELECT(GPIO_PIN_REG_15,FUNC_GPIO15);
-    xTaskCreate (&inter, "startup", 2048, NULL, 1, NULL);
-    printf("%s\n","Inicio prueba" );
-    //Configurar el PIN D8 (GPIO 15)
+  PIN_FUNC_SELECT(GPIO_PIN_REG_15,FUNC_GPIO15);
+xTaskCreate (&inter, "startup", 2048, NULL, 1, NULL);
+printf("%s\n","Inicio prueba" );
 
 
 }
